f987adf992828be10a924cb5702b0a6c6bb56ccc
VidTDM <realvidtdm@gmail.com>
2025-06-14 19:17:19 +0530

    fix breeze freezer capturing - VidTDM_XD

--- src/main/java/net/galaxyxidorn/cryofluid/component/ModDataComponents.java
+++ src/main/java/net/galaxyxidorn/cryofluid/component/ModDataComponents.java
@@ -3,21 +3,23 @@ package net.galaxyxidorn.cryofluid.component;
 import com.mojang.serialization.Codec;
 import net.galaxyxidorn.cryofluid.CryoFluid;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.UnaryOperator;
 
 public class ModDataComponents {
-    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(CryoFluid.MOD_ID);
+    public static final DeferredRegister.DataComponents DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, CryoFluid.MOD_ID);
 
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> CAPTURED = register("captured",
-            builder -> builder.persistent(Codec.BOOL));
+    public static final DataComponentType<Boolean> CAPTURED = register("captured",
+            builder -> builder.persistent(Codec.BOOL).networkSynchronized(ByteBufCodecs.BOOL));
 
-    private static <T> DeferredHolder<DataComponentType<?>, DataComponentType<T>> register(String name,
-                                                                                          UnaryOperator<DataComponentType.Builder<T>> builderOperator) {
-        return DATA_COMPONENT_TYPES.register(name, () -> builderOperator.apply(DataComponentType.builder()).build());
+    private static <T> DataComponentType<T> register(String name, UnaryOperator<DataComponentType.Builder<T>> builder) {
+        DataComponentType<T> type = builder.apply(DataComponentType.builder()).build();
+        DATA_COMPONENT_TYPES.register(name, () -> type);
+        return type;
     }
 
     public static void register(IEventBus bus) {
